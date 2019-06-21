8a2512770a3b6af524ddb4d8a806a9a5e5915fea
Mohamed chiheb ben jemaa <mc.benjemaa@gmail.com>
2019-06-21 09:27:29 +0100

    add config

---
+++ app/desktop/src/util/config.js
@@ -0,0 +1,9 @@
+Ext.define('extjsApp.util.Config', {
+    singleton : true,
+
+    config : {
+        baseAPIUrl : 'http://192.168.0.80:8080/api/',
+        metadataReqUrl: 'http://192.168.0.78:8085/services/request/getJsonData/1000534?param1=',
+   
+    }
+});
